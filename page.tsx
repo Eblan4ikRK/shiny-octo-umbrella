c096864eabf64280a1e33b07422fdb01e98cfdd2
botsfelo? <118761857+Eblan4ikRK@users.noreply.github.com>
2025-08-05 17:02:29 +0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-
-export default function HomePage() {
-    return (
-      <div>
-        <h1>Hello, my name is dexter, dexter morgan, i don't know the way who be i am but...</h1>
-      </div>
-    );
-  }
