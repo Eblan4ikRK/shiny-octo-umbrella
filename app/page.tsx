fe1143c684ba0cced32deda798c9c299964d2aff
botsfelo? <118761857+Eblan4ikRK@users.noreply.github.com>
2025-08-05 17:02:19 +0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Hello, my name is dexter, dexter morgan, i don't know the way who be i am but...</h1>
+      </div>
+    );
+  }
